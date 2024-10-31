09a1a57a262b22c2db2a41a71d2377a0786ec61a
jbdevelop3r <justinebanogon14@gmail.com>
2024-10-31 10:27:26 +0800

    filtered asia data market

--- app/controllers/finances_controller.rb
+++ app/controllers/finances_controller.rb
@@ -1,13 +1,10 @@
 class FinancesController < ApplicationController
-  def index 
+  def index
     if params[:query].present?
       service = SerpApi::GoogleFinanceService.new
-      @finances = service.get_stock_data(params[:query])
-
-      if @finances[:error]
-        flash[:alert] = "Could not fetch data: #{@finances[:message]}"
-        @finances = nil
-      end
+      data = service.get_stock_data(params[:query])
+      @finances = data["markets"]["asia"] if data["markets"] && data["markets"]["asia"]
+      @stock_symbol = data["search_parameters"]["q"]
     end
   end
 end
